31769f3544b69b035d44f503d83c597c0f948fd5
sajeedmehrab <ksmehrab@vt.edu>
2026-01-04 20:15:06 -0500

    arc scripts

--- ModelPlaygrounds/EasyR1/EasyR1/examples/qwen3_vl_think_8b_vrpart3_2x80GB_arc.sh
+++ ModelPlaygrounds/EasyR1/EasyR1/examples/qwen3_vl_think_8b_vrpart3_2x80GB_arc.sh
@@ -7,7 +7,7 @@ set -x
 MODEL_PATH=/projects/ml4science/kazi/VLMGroundingProject/PretrainedModels/Qwen3-VL-8B-Thinking # replace it with your local file path
 
 python3 -m verl.trainer.main \
-    config=examples/config.yaml \
+    config=/home/ksmehrab/GroundingProjectCodeRepo/ModelPlaygrounds/EasyR1/EasyR1/examples/config.yaml \
     data.train_files=/projects/ml4science/kazi/VLMGroundingProject/Datasets/SegZeroVR_InstructPart_Merged \
     data.val_files=/projects/ml4science/kazi/VLMGroundingProject/Datasets/SegZeroVR_InstructPart_Merged \
     data.format_prompt=/home/ksmehrab/GroundingProjectCodeRepo/ModelPlaygrounds/SegZero/EvaluationScripts/Prompts/vrpart2_prompt.txt \
@@ -15,4 +15,6 @@ python3 -m verl.trainer.main \
     trainer.experiment_name=qwen3vlthink8b_vrpart3_try1 \
     trainer.n_gpus_per_node=2 \
     trainer.save_checkpoint_path=/projects/ml4science/kazi/VLMGroundingProject/ModelData/EasyR1/qwen3vlthink8b_vrpart3_try1 \
-    trainer.total_epochs=2
+    trainer.total_epochs=2 \
+    trainer.save_freq=556 \
+    worker.reward.reward_function=/home/ksmehrab/GroundingProjectCodeRepo/ModelPlaygrounds/EasyR1/EasyR1/examples/reward_function/vision_reasoner_part3_easyr1.py:compute_score
